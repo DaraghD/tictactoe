68377463e2e0d040b2cb3a2cd6cb8cd4a9032163
DaraghD <daraghdownes12@gmail.com>
2022-10-15 15:50:47 +0100

    updating logic )

--- src/logic.py
+++ src/logic.py
@@ -1,6 +1,9 @@
 def checkWin(board,player):
     if board[0][0] + board[0][1] + board[0][2] == player*3:
-        return True
+        if board[0][0] == 0 or board[0][1] == 0 or board[0][2] == 0:
+            return False
+        else:
+            return True
     elif board[1][0] + board[1][1] + board[1][2] == player*3:
         return True
 
@@ -17,6 +20,7 @@ def checkWin(board,player):
         return True
 
     elif board[0][0] + board[1][1] + board[2][2] == player*3:
+
         return True
 
     elif board[2][0] + board[1][1] + board[0][2] == player*3:
--- src/main.py
+++ src/main.py
@@ -1,14 +1,24 @@
 import move
 import board
+import logic
 
 def main():
     while True:
+
+
         p1 = move.get_move()
         board.board[p1[0]][p1[1]] = 1
         board.printBoard()
 
+        if logic.checkWin(board.board,1):
+            print("player one won")
+            exit(1)
+
         p2 = move.get_move()
         board.board[p2[0]][p2[1]] = 2
         board.printBoard()
+        if logic.checkWin(board.board,2) :
+            print("player one lost")
+            exit(1)
 
 main()
