3174bf8bc2b78b5c3ec1b964f50af3a44bb55f2a
Mark Naughton <mark@marknaughton.com>
2022-10-15 15:54:28 +0100

    Check for empty in checkWin()

--- src/logic.py
+++ src/logic.py
@@ -4,25 +4,46 @@ def checkWin(board,player):
             return False
         else:
             return True
+
     elif board[1][0] + board[1][1] + board[1][2] == player*3:
-        return True
+        if board[1][0] == 0 or board[1][1] == 0 or board[1][2] == 0:
+            return False
+        else:
+            return True
 
     elif board[2][0] + board[2][1] + board[2][2] == player*3:
-        return True
+        if board[2][0] == 0 or board[2][1] == 0 or board[2][2] == 0:
+            return False
+        else:
+            return True
 
     elif board[0][0] + board[1][0] + board[2][0] == player*3:
-        return True
+        if board[0][0] == 0 or board[1][0] == 0 or board[2][0] == 0:
+            return False
+        else:
+            return True
 
     elif board[0][1] + board[1][1] + board[2][1] == player*3:
-        return True
+        if board[0][1] == 0 or board[1][1] == 0 or board[2][1] == 0:
+            return False
+        else:
+            return True
 
     elif board[0][2] + board[1][2] + board[2][2] == player*3:
-        return True
+        if board[0][2] == 0 or board[1][2] == 0 or board[2][2] == 0:
+            return False
+        else:
+            return True
 
     elif board[0][0] + board[1][1] + board[2][2] == player*3:
+        if board[0][0] == 0 or board[1][1] == 0 or board[2][2] == 0:
+            return False
+        else:
+            return True
 
-        return True
-
-    elif board[2][0] + board[1][1] + board[0][2] == player*3:
-        return True
+    elif board[2][0] + board[0][1] + board[0][2] == player*3:
+        if board[2][0] == 0 or board[0][1] == 0 or board[0][2] == 0:
+            return False
+        else:
+            return True
 
