808175903d726cdacc56e15c70fa29f9b0a5e29d
DaraghD <daraghdownes12@gmail.com>
2022-10-15 15:35:20 +0100

    checkwin

---
+++ src/checkWin.py
@@ -0,0 +1,22 @@
+def checkWin(board,player):
+    if board[0][0] + board[0][1] + board[0][2] == player*3:
+        return True
+    elif board[1][0] + board[1][1] + board[1][2] == player*3:
+        return True
+
+    elif board[2][0] + board[2][1] + board[2][2] == player*3:
+        return True
+
+    elif board[0][0] + board[1][0] + board[2][0] == player*3:
+        return True
+    elif board[0][1] + board[1][1] + board[2][1] == player*3:
+        return True
+
+    elif board[0][2] + board[1][2] + board[2][2] == player*3:
+        return True
+
+    elif board[0][0] + board[1][1] + board[2][2] == player*3:
+           return True
+
+    elif board[2][0] + board[1][1] + board[0][2] == player*3:
+        return True
