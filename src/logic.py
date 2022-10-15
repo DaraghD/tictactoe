4a71041b362062f1fcac63f119e49d5d51a85fc7
DaraghD <daraghdownes12@gmail.com>
2022-10-15 15:58:04 +0100

    validatemove()

--- src/logic.py
+++ src/logic.py
@@ -1,49 +1,55 @@
-def checkWin(board,player):
-    if board[0][0] + board[0][1] + board[0][2] == player*3:
+def checkWin(board, player):
+    if board[0][0] + board[0][1] + board[0][2] == player * 3:
         if board[0][0] == 0 or board[0][1] == 0 or board[0][2] == 0:
             return False
         else:
             return True
 
-    elif board[1][0] + board[1][1] + board[1][2] == player*3:
+    elif board[1][0] + board[1][1] + board[1][2] == player * 3:
         if board[1][0] == 0 or board[1][1] == 0 or board[1][2] == 0:
             return False
         else:
             return True
 
-    elif board[2][0] + board[2][1] + board[2][2] == player*3:
+    elif board[2][0] + board[2][1] + board[2][2] == player * 3:
         if board[2][0] == 0 or board[2][1] == 0 or board[2][2] == 0:
             return False
         else:
             return True
 
-    elif board[0][0] + board[1][0] + board[2][0] == player*3:
+    elif board[0][0] + board[1][0] + board[2][0] == player * 3:
         if board[0][0] == 0 or board[1][0] == 0 or board[2][0] == 0:
             return False
         else:
             return True
 
-    elif board[0][1] + board[1][1] + board[2][1] == player*3:
+    elif board[0][1] + board[1][1] + board[2][1] == player * 3:
         if board[0][1] == 0 or board[1][1] == 0 or board[2][1] == 0:
             return False
         else:
             return True
 
-    elif board[0][2] + board[1][2] + board[2][2] == player*3:
+    elif board[0][2] + board[1][2] + board[2][2] == player * 3:
         if board[0][2] == 0 or board[1][2] == 0 or board[2][2] == 0:
             return False
         else:
             return True
 
-    elif board[0][0] + board[1][1] + board[2][2] == player*3:
+    elif board[0][0] + board[1][1] + board[2][2] == player * 3:
         if board[0][0] == 0 or board[1][1] == 0 or board[2][2] == 0:
             return False
         else:
             return True
 
-    elif board[2][0] + board[0][1] + board[0][2] == player*3:
+    elif board[2][0] + board[0][1] + board[0][2] == player * 3:
         if board[2][0] == 0 or board[0][1] == 0 or board[0][2] == 0:
             return False
         else:
             return True
 
+
+def validateMove(move, board):
+    if board[move[0]][move[0]] == 0:
+        return True
+    else:
+        return False
